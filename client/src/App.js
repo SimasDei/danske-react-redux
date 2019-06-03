f54d8e82317a8b4631c367bf35aa62edb50d34b0
SimasDei <simondeiman@gmail.com>
2019-06-03 20:18:51 +0300

    add React, Redux, Sass

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import './scss/main.scss';
+
+import Main from './components/layouts/Main';
+
+const App = () => (
+  <Fragment>
+    <Main />
+  </Fragment>
+);
+
+export default App;
---
+++ client/src/components/layouts/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main>
+      <h1>Main Component</h1>
+    </main>
+  )
+}
+
+export default Main
